6bc45511e289955fff6ebb1c3b6141f74442d8f3
Harrison Reed <reedharrison95@gmail.com>
2021-07-15 12:53:24 -0700

    commit on 'main'

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Harrison Reed's Portfolio!</h1>
+        <h4>I am seeking Software Product Coordinator Career Opportunities!</h4>
+        <h2>Thank You For Checking Out My Work!</h2>
+        <div className="clickable-links-card">
+        <a className="click-link" href="https://www.linkedin.com/in/harrison-reed-067436170/">Check Out My LinkedIn</a>
+        <br/>
+        <br />
+        <a className="click-link" href="https://github.com/HGreedo"> Check Out My Github </a>
+        <a className="email" href="mailto:reedharrison95@gmai.com"> Send me an email HERE</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
