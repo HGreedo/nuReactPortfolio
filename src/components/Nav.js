33983f3cf3822d7d2ba461466dc55f3785ef8a19
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 11:22:58 -0700

    commit on 'main'

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 const Nav = () => {
     return (
       <nav className="navbar">
-    <Link className="navbarData" to="/">Home</Link>
+    <Link className="navbarData" to="/Home">Home</Link>
     <Link className="navbarData" to="./pages/About-Me">About Me</Link>
-    <Link className="navbarData" to="./IdaShop">Ida Andersson Shop</Link>
-    <Link className="navbarData" to="./Resume">Resume</Link>
-    <Link className="navbarData" to="./ThirdPtyLinks">Links</Link>
+    <Link className="navbarData" to="./pages/IdaShop">Ida Andersson Shop</Link>
+    <Link className="navbarData" to=".pages/Resume">Resume</Link>
+    <Link className="navbarData" to="./pages/ThirdPtyLinks">Links</Link>
+  
       </nav>
     );
   };
