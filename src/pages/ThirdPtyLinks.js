3f909f95a57df72a8e3f83a5b170ef4df731bbfa
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 16:20:41 -0700

    commit on 'main'

--- src/pages/ThirdPtyLinks.js
+++ src/pages/ThirdPtyLinks.js
@@ -7,8 +7,10 @@ const ThirdPtyLinks = () => (
             <p>
             This is a working route I need to update accordingly 
             </p>
-            <a className="click-link" href="https://github.com/HGreedo"> Check Out My Github </a>
-            <a className="click-link" href="https://www.linkedin.com/in/harrison-reed-067436170/">Check Out My LinkedIn</a>
+            <div className="link-container"> 
+            <a className="link" href="https://github.com/HGreedo"> Check Out My Github </a>
+            <a className="link" href="https://www.linkedin.com/in/harrison-reed-067436170/">Check Out My LinkedIn</a>
+            </div>
   </div>
         );
 
