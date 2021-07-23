3cb74f92a98a8f89de5cd77933bc9c4696080b12
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 12:43:42 -0700

    commit on 'main'

--- src/pages/About-Me.js
+++ src/pages/About-Me.js
@@ -1,9 +1,12 @@
 import React from "react";
 import img from "../images/linkedin.png"
+import ('./aboutMe.css')
+
+
 function About() {
  return (
-    <div>
-            {<img alt ='profile-pic' src={img}/>    }
+    <div className="about-container">
+            {<img className="profile-pic" alt ='profile-pic' src={img}/>    }
             <h1>Hello, I am Harrison</h1>
             <p>
             This is a working route I need to update accordingly 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,10 @@ function Home() {
         <div className="App">
         <header className="App-header">
           <h1>Welcome to Harrison Reed's Portfolio!</h1>
-          <h4>I am seeking Software Product Coordinator Career Opportunities!</h4>
+          <h4>I am seeking a Product Coordinator position. After continuing my pursuit of learning Software Developer and graduating from U of Oregon’s Full Stack Coding Program, I want to transfer my skills as a brand and digital product storyteller to collaborate on a team and create products on a much larger scale. 
+Over the past year, I’ve dialed m skills as a software developer and journalist to create impactful products that leave a positive impact and generate revenue. Due to circumstances posed by the pandemic, I acquired a business license and teamed up with my business partner to create a hand-made apparel brand called “Ida Andersson Shop” that specializes in Swedish-crafted Cozy Wear for women. My role focused on learning Etsy, Shopify, Wix, Instagram, Pinterest, Google Analytics, and creating organic content that engaged with our customers and boosted sales. We made and sold 400 items in our first 12 months out of a small studio apartment - All by hand and even sold at boutiques in Portland.
+I want to take my skills and what I’ve learned as a journalist, as an entrepreneur, and as a growing software developer to create products that engage, inspire, and sell. 
+</h4>
           <h2>Thank You For Checking Out My Work!</h2>  
         </header>
       </div>
