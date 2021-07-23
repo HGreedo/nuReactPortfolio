d5c08c311eafdd1199c0255869e05ad9949daba1
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 13:58:24 -0700

    commit on 'main'

--- src/pages/IdaShop.js
+++ src/pages/IdaShop.js
@@ -1,11 +1,23 @@
 import React from "react";
+import ('./idashop.css')
 
 const IdaShop = () => (
-    <div>
-            {/* <img alt ='profile-pic' src={profilePic}/>    */}
+    <div className="idashop-header">
             <h1>Here is where I will talk about Ida Andersson shop experience</h1>
-            <p>
-            This is a working route I need to update accordingly 
+            <h3 className="shop-link"> 
+                <a href="https://www.idaanderssonshop.com/">Click Here to Check Out Ida Andersson Shop</a>
+            </h3>
+            <p className="idashop-bio">
+            Ida Andersson LLC is a hand-made, Swedish Cozy Wear apparel brand that specializes in bespoke and custom-made clothing for women. 
+            The brand was established in the wake of the Covid-19 Pandemic by Ida and Harry. Together, we sold over 400 uniquely hand-made items in the first 12-months of operation, as well as MOQ's to boutiques in the Portland Area.
+            <br/>
+            <br/>
+            My role in Ida Andersson Shop is heavily involved with web development, digital content creation, marketing, and networking. 
+            <br/>
+            I'm not afraid to get my hands dirty so I also do all the fabric cutting, trimming and preparation of garments to help with production efficiency.
+            <br/>
+            <br/>
+            This has been an incredibly rewarding experience. For both Ida and I, Ida Andersson LLC really was a 'proving ground' for both of us to combine our skils together to make something that we feel is successful!
             </p>
   </div>
         );
