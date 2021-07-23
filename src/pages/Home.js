6ef6c2ce8045654891736070ce4de58b3c9a6122
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 12:51:29 -0700

    commit on 'main'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,25 @@
+import ('./home.css')
+
 function Home() {
     return (
         <div className="App">
         <header className="App-header">
           <h1>Welcome to Harrison Reed's Portfolio!</h1>
-          <h4>I am seeking a Product Coordinator position. After continuing my pursuit of learning Software Developer and graduating from U of Oregon’s Full Stack Coding Program, I want to transfer my skills as a brand and digital product storyteller to collaborate on a team and create products on a much larger scale. 
-Over the past year, I’ve dialed m skills as a software developer and journalist to create impactful products that leave a positive impact and generate revenue. Due to circumstances posed by the pandemic, I acquired a business license and teamed up with my business partner to create a hand-made apparel brand called “Ida Andersson Shop” that specializes in Swedish-crafted Cozy Wear for women. My role focused on learning Etsy, Shopify, Wix, Instagram, Pinterest, Google Analytics, and creating organic content that engaged with our customers and boosted sales. We made and sold 400 items in our first 12 months out of a small studio apartment - All by hand and even sold at boutiques in Portland.
+          <h5 className="home-blurb">I want to use my skills as a brand and digital product storyteller to collaborate on a team in order create compelling, inspiring, demand-driving products on a much larger scale. 
+              <br/>
+              <br/>
+Over the past year, I’ve dialed m skills as a software developer and journalist to create impactful products that leave a positive impact and generate revenue. 
+<br/>
+Due to circumstances posed by the pandemic, I acquired a business license and teamed up with my business partner to create a hand-made apparel brand called “Ida Andersson Shop” that specializes in Swedish-crafted Cozy Wear for women. My role focused on learning Etsy, Shopify, Wix, Instagram, Pinterest, Google Analytics, and creating organic content that engages with our customers and boostw sales. 
+<br/>
+<br/>
+We made and sold 400 items in our first 12 months out of a small studio apartment - All by hand and even sold at boutiques in Portland.
+<br/>
+<br/>
 I want to take my skills and what I’ve learned as a journalist, as an entrepreneur, and as a growing software developer to create products that engage, inspire, and sell. 
-</h4>
-          <h2>Thank You For Checking Out My Work!</h2>  
+</h5>
+
+<h2>Thank You For Checking Out My Work!</h2>  
         </header>
       </div>
   
