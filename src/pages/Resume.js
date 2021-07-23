aab3cb71797c4ea82189278acdbc4453fab3df07
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 16:32:19 -0700

    commit on 'main'

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -3,8 +3,8 @@ import img from "../images/HReedResume.png"
 import ('./resume.css')
 
 const Resume = () => (
-    <div className="resume-container">
-            <img alt ='resume' src={img}/>   
+    <div>
+            <img className="resume-container" alt ='resume' src={img}/>   
             
   </div>
         );
