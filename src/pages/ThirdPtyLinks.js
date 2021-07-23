3102e0455c65e809e4ef0972930f45a495996c2b
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 16:54:10 -0700

    commit on 'main'

--- src/pages/ThirdPtyLinks.js
+++ src/pages/ThirdPtyLinks.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ('./thirdpartylinks.css')
 const ThirdPtyLinks = () => (
     <div>
             {/* <img alt ='profile-pic' src={profilePic}/>    */}
@@ -7,10 +7,35 @@ const ThirdPtyLinks = () => (
             <p>
             This is a working route I need to update accordingly 
             </p>
-            <div className="link-container"> 
-            <a className="link" href="https://github.com/HGreedo"> Check Out My Github </a>
+            <ul className="link-container"> 
+            
+            <li className="link">
+            <a href="https://github.com/HGreedo">Check Out My Github</a>
+            </li>
+            <li className="link">
             <a className="link" href="https://www.linkedin.com/in/harrison-reed-067436170/">Check Out My LinkedIn</a>
-            </div>
+            </li>
+            <li className="link">
+            <a className="email" href="mailto:reedharrison95@gmai.com">reedharrison95@gmail.com</a>
+            </li>
+
+            <li className="link">
+            <a href="https://finalproject-ofbc.herokuapp.com/">Final Project "OFBC" *still in development*</a>
+            </li>
+            <li className="link">
+            <a href="https://www.idaanderssonshop.com/">Ida Andersson Shop</a>
+            </li>
+            <li className="link">
+            <a className="email" href="mailto:idaanderssonshop@gmai.com">idaanderssonshop@gmail.com</a>
+            </li>
+            <li className="link">
+            <a href="https://www.etsy.com/shop/IdaAnderssonShop">Reviews on Etsy</a>
+            </li>
+            <li className="link">
+            <a href="https://github.com/HGreedo?tab=repositories">School Work Repos</a>
+            </li>
+
+            </ul>
   </div>
         );
 
