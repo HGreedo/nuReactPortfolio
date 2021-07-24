2f6b1829bb0e7a823ffd03a16393d7031886d390
Harrison Reed <reedharrison95@gmail.com>
2021-07-23 17:05:43 -0700

    commit on 'main'

--- src/pages/ThirdPtyLinks.js
+++ src/pages/ThirdPtyLinks.js
@@ -2,13 +2,9 @@ import React from "react";
 import ('./thirdpartylinks.css')
 const ThirdPtyLinks = () => (
     <div>
-            {/* <img alt ='profile-pic' src={profilePic}/>    */}
-            <h1>Here is where I will put my work examples</h1>
-            <p>
-            This is a working route I need to update accordingly 
-            </p>
             <ul className="link-container"> 
-            
+    
+    <div className="personal-stuff"> 
             <li className="link">
             <a href="https://github.com/HGreedo">Check Out My Github</a>
             </li>
@@ -18,25 +14,55 @@ const ThirdPtyLinks = () => (
             <li className="link">
             <a className="email" href="mailto:reedharrison95@gmai.com">reedharrison95@gmail.com</a>
             </li>
+        </div>
 
+<div className="school-projects">
             <li className="link">
             <a href="https://finalproject-ofbc.herokuapp.com/">Final Project "OFBC" *still in development*</a>
             </li>
+
+            <li className="link">
+            <a href="https://pdxbellasaurus.github.io/superheroBirthdayBash/">Hero Birthday Bash Project</a>
+            <a href="https://docs.google.com/presentation/d/1YN1dKxGTsNWpa24_HdFn7iy1BhZx8Ref-P-BbM_Nm-k/edit?usp=sharing">Presentation</a>
+            </li>
+
+            <li className="link">
+            <a href="https://github.com/HGreedo/momentous">Momentous Project</a>
+            </li>
+            <li className="link">
+            <a href="https://docs.google.com/presentation/d/1mUAch6rXh79rro9NWX30VUXOumrSwu9DyHqwXFJlAQQ/edit#slide=id.p">Momentous Presentation</a>
+            </li>
+            </div>
+
+
+
+<div className="ida-shop"> 
             <li className="link">
             <a href="https://www.idaanderssonshop.com/">Ida Andersson Shop</a>
             </li>
+
             <li className="link">
             <a className="email" href="mailto:idaanderssonshop@gmai.com">idaanderssonshop@gmail.com</a>
             </li>
+            
             <li className="link">
             <a href="https://www.etsy.com/shop/IdaAnderssonShop">Reviews on Etsy</a>
             </li>
+
             <li className="link">
             <a href="https://github.com/HGreedo?tab=repositories">School Work Repos</a>
             </li>
+            </div>
+
 
             </ul>
   </div>
         );
 
 export default ThirdPtyLinks;
+
+
+
+
+
+
