be7e0c83ccc085149e6571e7b8e73b3ea0012644
Harrison Reed <reedharrison95@gmail.com>
2021-07-24 21:08:55 -0700

    commit on 'main'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,15 +5,15 @@ function Home() {
         <div className="App">
         <header className="App-header">
           <h1>Welcome to Harry Reed's Portfolio!</h1>
-          <h5 className="home-blurb">I want to use my skills as a brand and digital product storyteller to foster collaboration as a Digital Product Management Specialist for eCommerce Platforms.
+          <h5 className="home-blurb">I want to use my skills as a brand and digital product storyteller to foster collaboration as a Digital Product Management Specialist - eCommerce Platforms.
               <br/>
               <br/>
-Over the past year, I’ve dialed my skills as a software developer and journalist to create impactful products that leave a positive impact and generate revenue. 
+Over the past year, I’ve dialed my skills as a web developer and digital marketer to create impactful products that engage consumers and generate revenue. 
 <br/>
 
 <br/>
 <br/>
-I want to use my skills my skills as a journalist, an entrepreneur, and as a growing software developer to create products that engages, inspires, and drives revenue. 
+My goal as a journalist, an entrepreneur, and as a growing software developer is to create products that engage, inspire, and drive positive action. 
 </h5>
 
 <h2>Thank You For Checking Out My Work!</h2>  
@@ -24,3 +24,5 @@ I want to use my skills my skills as a journalist, an entrepreneur, and as a gro
 }
 
 export default Home;
+
+
