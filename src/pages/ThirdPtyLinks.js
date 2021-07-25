35b94fdcbe50bf98aadfebf52808182c0540972d
Harrison Reed <reedharrison95@gmail.com>
2021-07-24 18:53:47 -0700

    commit on 'main'

--- src/pages/ThirdPtyLinks.js
+++ src/pages/ThirdPtyLinks.js
@@ -3,53 +3,38 @@ import ('./thirdpartylinks.css');
 
 const ThirdPtyLinks = () => (
     <div className="link-body">
-            <ul className="link-container"> 
+            
     
 
     <div className="container"> <h3>Github/LinkedIn/Email</h3>
-            <li>
+            
             <a className="atag" href="https://github.com/HGreedo">Github</a>
-            </li>
-            <li>
+            
+            
             <a className="atag"  href="https://www.linkedin.com/in/harrison-reed-067436170/">LinkedIn</a>
-            </li>
-            <li>
+            
             <a className="atag" href="mailto:reedharrison95@gmai.com">reedharrison95@gmail.com</a>
-            </li>
+           
         </div>
 
 <div className="container">  <h3>Full Stack Coding School Projects</h3>
-            <li >
-            <a className="atag" href="https://finalproject-ofbc.herokuapp.com/">Final Project "OFBC" *still in development*</a>
-            </li>
-
-            <li>
-            <a className="atag" href="https://pdxbellasaurus.github.io/superheroBirthdayBash/">"Hero Birthday Bash" Project</a>
-            </li>
-
-            <li >
-            <a className="atag" href="https://docs.google.com/presentation/d/1YN1dKxGTsNWpa24_HdFn7iy1BhZx8Ref-P-BbM_Nm-k/edit?usp=sharing">"Birthday Bash" Presentation</a>
-            </li>
-
-            <li >
-            <a className="atag" href="https://github.com/HGreedo/momentous">"Momentous" Project</a>
-            </li>
-
-            <li >
-            <a className="atag" href="https://docs.google.com/presentation/d/1mUAch6rXh79rro9NWX30VUXOumrSwu9DyHqwXFJlAQQ/edit#slide=id.p">"Momentous" Presentation</a>
-            </li>
-
-            <li >
-            <a className="atag" href="https://github.com/HGreedo?tab=repositories">Assignment Repositories</a>
-            </li>
             
-            <li >
-            <a className="atag" href="https://www.youtube.com/channel/UCvZV5eUFMovWcflaRRrIMCA/videos">Univ. of Arizona Journalism Video Work</a>
-            </li>
+            <a className="atag" href="https://finalproject-ofbc.herokuapp.com/">Final Project "OFBC" *still in development*</a>
+           
 
-            <li >
+            <a className="atag" href="https://pdxbellasaurus.github.io/superheroBirthdayBash/">"Hero Birthday Bash" Project</a>
+
+            <a className="atag" href="https://docs.google.com/presentation/d/1YN1dKxGTsNWpa24_HdFn7iy1BhZx8Ref-P-BbM_Nm-k/edit?usp=sharing">"Birthday Bash" Presentation</a>
+
+            <a className="atag" href="https://github.com/HGreedo/momentous">"Momentous" Project</a>
+
+            <a className="atag" href="https://docs.google.com/presentation/d/1mUAch6rXh79rro9NWX30VUXOumrSwu9DyHqwXFJlAQQ/edit#slide=id.p">"Momentous" Presentation</a>
+
+            <a className="atag" href="https://github.com/HGreedo?tab=repositories">Assignment Repositories</a>
+            
+            <a className="atag" href="https://www.youtube.com/channel/UCvZV5eUFMovWcflaRRrIMCA/videos">Univ. of Arizona Journalism Video Work</a>
+
             <a  className="atag" href="https://indearizona.com/tucson-tattoo-culture-oasis-for-art-expression-in-southwest/">Tattoo Tourism Story - "El Inde Arizona"</a>
-            </li>
 
             </div>
 
@@ -57,23 +42,16 @@ const ThirdPtyLinks = () => (
 
 <div className="container">  <h3>Ida Andersson Shop Details</h3>
 
-            <li >
             <a className="atag" href="https://www.idaanderssonshop.com/">Ida Andersson Shop</a>
-            </li>
 
-            <li >
             <a className="atag"  href="mailto:idaanderssonshop@gmai.com">idaanderssonshop@gmail.com</a>
-            </li>
             
-            <li >
             <a className="atag" href="https://www.etsy.com/shop/IdaAnderssonShop">Reviews on Etsy</a>
-            </li>
 
             
             </div>
 
 
-            </ul>
   </div>
         );
 
