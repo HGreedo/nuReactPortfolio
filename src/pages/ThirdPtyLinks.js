12bf3fa1ef28b35cc6b4ec299d4eb92b3204b8dd
Harrison Reed <reedharrison95@gmail.com>
2021-07-24 18:28:22 -0700

    commit on 'main'

--- src/pages/ThirdPtyLinks.js
+++ src/pages/ThirdPtyLinks.js
@@ -7,47 +7,47 @@ const ThirdPtyLinks = () => (
     
 
     <div className="container"> <h3>Github/LinkedIn/Email</h3>
-            <li className="link">
+            <li>
             <a className="atag" href="https://github.com/HGreedo">Github</a>
             </li>
-            <li className="link">
+            <li>
             <a className="atag"  href="https://www.linkedin.com/in/harrison-reed-067436170/">LinkedIn</a>
             </li>
-            <li className="link">
+            <li>
             <a className="atag" href="mailto:reedharrison95@gmai.com">reedharrison95@gmail.com</a>
             </li>
         </div>
 
 <div className="container">  <h3>Full Stack Coding School Projects</h3>
-            <li className="link">
+            <li >
             <a className="atag" href="https://finalproject-ofbc.herokuapp.com/">Final Project "OFBC" *still in development*</a>
             </li>
 
-            <li className="link">
+            <li>
             <a className="atag" href="https://pdxbellasaurus.github.io/superheroBirthdayBash/">"Hero Birthday Bash" Project</a>
             </li>
 
-            <li className="link">
+            <li >
             <a className="atag" href="https://docs.google.com/presentation/d/1YN1dKxGTsNWpa24_HdFn7iy1BhZx8Ref-P-BbM_Nm-k/edit?usp=sharing">"Birthday Bash" Presentation</a>
             </li>
 
-            <li className="link">
+            <li >
             <a className="atag" href="https://github.com/HGreedo/momentous">"Momentous" Project</a>
             </li>
 
-            <li className="link">
+            <li >
             <a className="atag" href="https://docs.google.com/presentation/d/1mUAch6rXh79rro9NWX30VUXOumrSwu9DyHqwXFJlAQQ/edit#slide=id.p">"Momentous" Presentation</a>
             </li>
 
-            <li className="link">
+            <li >
             <a className="atag" href="https://github.com/HGreedo?tab=repositories">Assignment Repositories</a>
             </li>
             
-            <li className="link">
+            <li >
             <a className="atag" href="https://www.youtube.com/channel/UCvZV5eUFMovWcflaRRrIMCA/videos">Univ. of Arizona Journalism Video Work</a>
             </li>
 
-            <li className="link">
+            <li >
             <a  className="atag" href="https://indearizona.com/tucson-tattoo-culture-oasis-for-art-expression-in-southwest/">Tattoo Tourism Story - "El Inde Arizona"</a>
             </li>
 
@@ -57,15 +57,15 @@ const ThirdPtyLinks = () => (
 
 <div className="container">  <h3>Ida Andersson Shop Details</h3>
 
-            <li className="link">
+            <li >
             <a className="atag" href="https://www.idaanderssonshop.com/">Ida Andersson Shop</a>
             </li>
 
-            <li className="link">
+            <li >
             <a className="atag"  href="mailto:idaanderssonshop@gmai.com">idaanderssonshop@gmail.com</a>
             </li>
             
-            <li className="link">
+            <li >
             <a className="atag" href="https://www.etsy.com/shop/IdaAnderssonShop">Reviews on Etsy</a>
             </li>
 
