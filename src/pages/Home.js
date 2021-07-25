5fdf67ecdabc0dda2539c690930bd0afee49fd31
Harrison Reed <reedharrison95@gmail.com>
2021-07-24 17:54:12 -0700

    commit on 'main'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,15 @@ function Home() {
         <div className="App">
         <header className="App-header">
           <h1>Welcome to Harry Reed's Portfolio!</h1>
-          <h5 className="home-blurb">I want to use my skills as a brand and digital product storyteller to collaborate on a team in order create compelling, inspiring, and demand-driving products on a much larger scale. 
+          <h5 className="home-blurb">I want to use my skills as a brand and digital product storyteller to foster collaboration as a Digital Product Management Specialist for eCommerce Platforms.
               <br/>
               <br/>
 Over the past year, I’ve dialed my skills as a software developer and journalist to create impactful products that leave a positive impact and generate revenue. 
 <br/>
-Due to circumstances posed by the pandemic, I acquired a business license and teamed up with my business partner to create a hand-made apparel brand called “Ida Andersson Shop” that specializes in Swedish-crafted Cozy Wear for women. 
-<br/>
-My role focuses on web development and brand marketing through creating organic content that inspires our customers and drive sales. 
+
 <br/>
 <br/>
-We made and sold 400 items in our first 12 months out of a small studio apartment - All by hand and even sold at boutiques in Portland.
-<br/>
-<br/>
-I want to take my skills and what I’ve learned as a journalist, as an entrepreneur, and as a growing software developer to create products that engage, inspire, and sell. 
+I want to use my skills my skills as a journalist, an entrepreneur, and as a growing software developer to create products that engages, inspires, and drives revenue. 
 </h5>
 
 <h2>Thank You For Checking Out My Work!</h2>  
