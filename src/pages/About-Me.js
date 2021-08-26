1ac984b248983837e19d68773c658b7b68e41f88
Harrison Reed <reedharrison95@gmail.com>
2021-08-26 12:21:32 -0700

    commit on 'main'

--- src/pages/About-Me.js
+++ src/pages/About-Me.js
@@ -16,25 +16,27 @@ function About() {
                 <br/>
                 <br/>                
                 <br/>
-                Starting Ida Andersson Shop, learning how to incorporate Google SEO/SEM, Etsy, Wix, Wordpress, Google Analytics / Tag Manager, Pinterest, and Instagram, and evolved into this passion and insatiable desire to learn more about producing and marketing digital content and managing it all.
+                Starting Ida Andersson Shop, learning how to incorporate Google SEO/SEM, Etsy, Wix, Wordpress, Google Analytics / Tag Manager, Pinterest, and Instagram, evolved into this passion and insatiable desire to learn and create effective digital content and successfully market it!
                 <br/>
                 <br/>
-                Ultimately, my love for 'organic' digital content led me to embarking on this software coding journey and the 6-month Full Stack Flex Bootcamp through U of Oregon.
+                Ultimately, my love for building 'organic' digital content led me to embark on this software coding journey and the Full Stack Web Deveopment Bootcamp through U of Oregon.
                 <br/>
                 <br/>
                 The Bootcamp was a fantastic platform to develop my computational skills, theory, and application, while also combining my skills as a producer and marketer to create 1 of 1 concepts in the form of Final Projects! 
                 <br/>
                 <br/>
-                I am a huge risk taker, big believer in "You make zero percent of the shots you don't take," and my new favorite is "Light Yourself on Fire and Find People Who Fuel Your Flames."
+                <br/>
+                <br/>
+                I am a huge risk taker, big believer in "You make zero percent of the shots you don't take," and my favorite quote that I've been running with since Covid is, "Light Yourself on Fire and Find People Who Fuel Your Flames." For me, that means collaborate, listen to others, and foster community. 
                 <br />
                 <br/>
-                Unlike my peers who were paired in groups of 4-5, I opted to do my Final Project completely own my own. 
+                One unique experience, was that, unlike my peers who were paired in groups of 4-5, I opted to do my Final Project completely own my own. 
                 <br/>
                 <br/>
                 I conducted my own market research, ran a 'mini focus group' with students at the U of Oregon Sports Product Masters Program, and built the entire backend / frontend configuration, as well as styling and routing on my own.
                 <br/>
                 <br/>
-                It may still be in 'development', but the entire process and the challenges of learning and applying "on-the-spot, " was an experience that gave me confidence to know that I can do this professionally!
+                The entire process and the challenges of learning and applying "on-the-spot, " was an experience that gave me confidence and the wherewithal to know that I can do this as a career!
                 <br/>
                 <br/>
                 <a className="OFBC" href="https://docs.google.com/presentation/d/11norT5-tjyDEH1t3zuUZNtmzWBwMC2yjOdkXTDBOoX0/edit?usp=sharing">You can check out my presentation on "OFBC" here</a>
