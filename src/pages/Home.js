80cc98d655707777f69bc27c3e7c4be55374372f
Harrison Reed <reedharrison95@gmail.com>
2021-08-26 12:23:31 -0700

    commit on 'main'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,11 @@ function Home() {
 <br/>
 <br/>
 <br/>
-My goal as a journalist, a small-business owner, and as a growing software developer is to create digital products that go beyond engaging and inspiring people. My purpose is to create stories that change how people perceive the world for the better, and driving positive action
+My goal as a journalist, a small-business owner, and as a growing software developer is to create digital content and products that go beyond engaging and inspiring people. 
+<br/>
+<br/>
+<br/>
+My purpose is to create stories that change how people perceive the world for the better, and driving positive action
 </h5>
 
 <h2>Thank You For Checking Out My Work!</h2>  
